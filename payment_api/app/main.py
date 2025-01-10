492bfce56bf529b38a42b702771bc2266e18e063
Adigun Jide Idris <edijman@gmail.com>
2025-01-10 15:27:29 -0500

    Add CORS middleware to FastAPI application for cross-origin requests

--- payment_api/app/main.py
+++ payment_api/app/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
 from app.routers import payments, file, db
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(payments.router, prefix="/payments", tags=["Payments"])
 app.include_router(file.router, prefix="/evidence", tags=["File Upload"])
 app.include_router(db.router, prefix="/db", tags=["Database Init"])
-
