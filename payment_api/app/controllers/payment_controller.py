96b3706ae20bce6dc2fe5573228b30d6bf908d69
Adigun Jide Idris <edijman@gmail.com>
2025-01-10 16:40:52 -0500

    Enhance payment retrieval to include total pages and improve response structure

--- payment_api/app/controllers/payment_controller.py
+++ payment_api/app/controllers/payment_controller.py
@@ -9,16 +9,19 @@ async def get_payments_controller(status: Optional[str] = None, page: int = 1, l
     if status:
         query["payee_payment_status"] = status
 
-    payments = payments_collection.find(query).skip(skip).limit(limit).to_list(length=limit)
+    total_payments = payments_collection.count_documents(query)
+    payments =  payments_collection.find(query).skip(skip).limit(limit).to_list(length=limit)
     for payment in payments:
         payment["_id"] = str(payment["_id"])
     if not payments:
         raise HTTPException(status_code=404, detail="No payments found")
-    return payments
+    
+    total_pages = (total_payments + limit - 1) // limit  # Calculate total number of pages
+    return {"payments": payments, "total_pages": total_pages}
 
 
 async def create_payment_controller(payment: PaymentCreate):
-    result = await payments_collection.insert_one(payment.dict())
+    result =  payments_collection.insert_one(payment.dict())
     if result.inserted_id:
         return {"id": str(result.inserted_id)}
     else:
@@ -28,7 +31,7 @@ async def create_payment_controller(payment: PaymentCreate):
 async def update_payment_controller(payment_id: str, payment_update: PaymentUpdate):
     if payment_update.status == "completed" and not payment_update.evidence_file:
         raise HTTPException(400, "Evidence file required for 'completed' status")
-    result = await payments_collection.update_one({"_id": payment_id}, {"$set": payment_update.dict()})
+    result =  payments_collection.update_one({"_id": payment_id}, {"$set": payment_update.dict()})
     if result.modified_count > 0:
         return {"success": True}
     else:
